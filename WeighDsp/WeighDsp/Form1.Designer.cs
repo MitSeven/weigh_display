51811e38113126b7f7a7a297c2126a5207ec8e67
MitSeven <nguyenphuongtien@outlook.com>
2018-12-09 11:08:05 +0700

    v1.0

---
+++ WeighDsp/WeighDsp/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace WeighDsp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.cbCom = new System.Windows.Forms.ComboBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.txtSend = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtRcv = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // cbCom
+            // 
+            this.cbCom.FormattingEnabled = true;
+            this.cbCom.Location = new System.Drawing.Point(281, 51);
+            this.cbCom.Name = "cbCom";
+            this.cbCom.Size = new System.Drawing.Size(121, 21);
+            this.cbCom.TabIndex = 0;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(281, 10);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(121, 23);
+            this.btnConnect.TabIndex = 1;
+            this.btnConnect.Text = "Kết nối";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // txtSend
+            // 
+            this.txtSend.Location = new System.Drawing.Point(12, 251);
+            this.txtSend.Name = "txtSend";
+            this.txtSend.Size = new System.Drawing.Size(182, 20);
+            this.txtSend.TabIndex = 2;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(200, 248);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 3;
+            this.btnSend.Text = "Gửi";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txtRcv
+            // 
+            this.txtRcv.Location = new System.Drawing.Point(12, 12);
+            this.txtRcv.Multiline = true;
+            this.txtRcv.Name = "txtRcv";
+            this.txtRcv.Size = new System.Drawing.Size(263, 233);
+            this.txtRcv.TabIndex = 4;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 300;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(414, 283);
+            this.Controls.Add(this.txtRcv);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtSend);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.cbCom);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.ComboBox cbCom;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.TextBox txtSend;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox txtRcv;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ WeighDsp/WeighDsp/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading;
+
+namespace WeighDsp
+{
+    public partial class Form1 : Form
+    {
+        Thread thr;
+        string weigh = ".000000";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort1.PortName = cbCom.Text;
+            }
+            catch { }
+            if (!serialPort1.IsOpen)
+            {
+                serialPort1.Open();
+                serialPort1.DataReceived += SerialPort1_DataReceived;
+              }
+            else
+            {
+                try
+                {
+                    serialPort1.Close();
+                }
+                catch { }
+            }
+            btnConnect.Text = serialPort1.IsOpen ? "Ngắt kết nối" : "Kết nối";
+            cbCom.Enabled= serialPort1.IsOpen ? false : true;
+        }
+
+        private void SerialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            txtRcv.Invoke(new Action(() =>
+            {
+                txtRcv.Text +="\nNhận: "+ serialPort1.ReadLine()+"\n";
+            }));
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cbCom.DataSource = SerialPort.GetPortNames();
+            serialPort1.BaudRate = 1200;
+            serialPort1.StopBits = StopBits.One;
+            serialPort1.Parity = Parity.None;
+            serialPort1.DataBits = 8;
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            txtRcv.Text += "\nGửi: " + txtSend.Text+"\n";
+            weigh = txtSend.Text;
+            txtSend.Clear();
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                    if ((txtSend.Text.Length == 0))
+                    {
+                        serialPort1.Write(weigh + "0=");
+                    }
+                    else
+                    {
+                        serialPort1.Write(".0000000=");
+                    }
+            }
+        }
+    }
+}
